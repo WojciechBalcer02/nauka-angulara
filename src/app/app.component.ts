62c6b59d6ac0720c049fb8da6539c3adfb16b98f
WojciechBalcer02 <wojciechbalcer02@gmail.com>
2023-01-11 19:49:36 +0100

    naukaAngulara

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nauka-angulara';
+  tasks = 'Brak zadań, masz czas wolny!'
+  tasksList: Array<string> = [];
+  tasksListDone: Array<string> = [];
+  newTask: string = ''; 
+  add(){
+    this.tasksList.push(this.newTask);
+    this.newTask = '';
+  }
+  remove(task:string){
+    this.tasksList = this.tasksList.filter(e=>e !==task);
+  }
+  done(task:string){
+    this.tasksListDone.push(task);
+    this.remove(task);
+  }
 }
